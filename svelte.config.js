263ded6fc56df5aa492c506c1f5ed3602cdecbc3
neoan <stefan.roehrl@gmail.com>
2024-11-28 15:34:41 -0500

    adapt environment

--- svelte.config.js
+++ svelte.config.js
@@ -20,7 +20,7 @@ const config = {
       precompress: false
     }),
     paths: {
-      base: dev ? '' : '/apply.recrew.de'
+      base: dev ? '' : '/apply.avantgarde.celest.rest'
     }
   },
 };
